7dc5825507585485a0f777a284de7166cd328ab4
ssspokd <pok86@mail.ru>
2016-08-31 19:23:50 +0300

    Initial commit

---
+++ app/src/main/java/com/directory/ssspokd/dictioanryconsultant/FNSService.java
@@ -0,0 +1,16 @@
+package com.directory.ssspokd.dictioanryconsultant;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FNSService extends Service {
+    public FNSService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/directory/ssspokd/dictioanryconsultant/gh.java
@@ -0,0 +1,10 @@
+package com.directory.ssspokd.dictioanryconsultant;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ssspokd on 30.08.2016.
+ */
+public class gh extends AppCompatActivity {
+
+}
